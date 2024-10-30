617a99d355622471b5802b47036939acd01b09ad
Luis Castilla <159273408+luiscastillaPro@users.noreply.github.com>
2024-10-30 13:48:20 +0000

    se agregan animaciones

--- src/front/js/pages/Skills.js
+++ src/front/js/pages/Skills.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../styles/skills.css";
 import html from "../../img/html-logo.png";
 import css from "../../img/css-logo.png";
@@ -17,39 +17,58 @@ import canva from "../../img/canva-logo.png";
 import wordpress from "../../img/wordpress-logo.png";
 
 const Skills = () => {
+    useEffect(() => {
+        const handleScroll = () => {
+            const fadeElements = document.querySelectorAll(".fade-in");
+            fadeElements.forEach((element) => {
+                const rect = element.getBoundingClientRect();
+                if (rect.top < window.innerHeight - 100) {
+                    element.classList.add("visible");
+                } else {
+                    element.classList.remove("visible");
+                }
+            });
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        handleScroll(); // Ejecuta al cargar para elementos ya en pantalla
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <div id="skills" className="skills-container"> {/* Aquí está el ID */}
-            <h2 className="skills-title">My Skills</h2>
-            <div className="skill-cont-table">
+        <div id="skills" className="skills-container"> 
+            <h2 className="skills-title fade-in">My Skills</h2>
+            <div className="skill-cont-table fade-in">
                 <table className="skills-table">
-                    <tbody> {/* Cambié a tbody para una mejor semántica */}
+                    <tbody>
                         <tr>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={html} alt="HTML" />
                                     <span className="skill-text">HTML5</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={css} alt="CSS" />
                                     <span className="skill-text">CSS3</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={javascript} alt="JavaScript" />
                                     <span className="skill-text">JavaScript</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={react} alt="React" />
                                     <span className="skill-text">React</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={figma} alt="Figma" />
                                     <span className="skill-text">Figma</span>
                                 </div>
@@ -57,31 +76,31 @@ const Skills = () => {
                         </tr>
                         <tr>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={node} alt="Node.js" />
                                     <span className="skill-text">Node.js</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={git} alt="Git" />
                                     <span className="skill-text">Git</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={github} alt="GitHub" />
                                     <span className="skill-text">GitHub</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={bootstrap} alt="Bootstrap" />
                                     <span className="skill-text">Bootstrap</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={canva} alt="Canva" />
                                     <span className="skill-text">Canva</span>
                                 </div>
@@ -89,31 +108,31 @@ const Skills = () => {
                         </tr>
                         <tr>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={python} alt="Python" />
                                     <span className="skill-text">Python</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={sql} alt="SQL" />
                                     <span className="skill-text">SQL</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={flask} alt="Flask" />
                                     <span className="skill-text">Flask</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={jwt} alt="JWT" />
                                     <span className="skill-text">JWT</span>
                                 </div>
                             </td>
                             <td>
-                                <div className="skill-item">
+                                <div className="skill-item fade-in">
                                     <img src={wordpress} alt="WordPress" />
                                     <span className="skill-text">WordPress</span>
                                 </div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,103 +6,123 @@ import { faLinkedin, faGithub, faInstagram, faFacebook } from '@fortawesome/free
 import Skills from "../pages/Skills.js";
 
 const Home = () => {
-	const [displayText, setDisplayText] = useState("");
-	const [cursorVisible, setCursorVisible] = useState(true);
-	const fullText = "Full Stack Developer";
-	const typingSpeed = 150;
-	const deletingSpeed = 150;
-	const pauseTime = 1000;
-	const cursorBlinkSpeed = 500;
+    const [displayText, setDisplayText] = useState("");
+    const [cursorVisible, setCursorVisible] = useState(true);
+    const fullText = "Full Stack Developer";
+    const typingSpeed = 150;
+    const deletingSpeed = 150;
+    const pauseTime = 1000;
+    const cursorBlinkSpeed = 500;
 
-	useEffect(() => {
-		let index = 0;
-		let isDeleting = false;
-		const typeEffect = () => {
-			if (index < fullText.length && !isDeleting) {
-				setDisplayText((prev) => prev + fullText.charAt(index));
-				index++;
-				setTimeout(typeEffect, typingSpeed);
-			} else if (index === fullText.length && !isDeleting) {
-				setTimeout(() => {
-					isDeleting = true;
-					typeEffect();
-				}, pauseTime);
-			} else if (index > 0 && isDeleting) {
-				setDisplayText((prev) => prev.slice(0, -1));
-				index--;
-				setTimeout(typeEffect, deletingSpeed);
-			} else if (index === 0 && isDeleting) {
-				isDeleting = false;
-				setTimeout(typeEffect, pauseTime);
-			}
-		};
-		typeEffect();
+    useEffect(() => {
+        let index = 0;
+        let isDeleting = false;
+        const typeEffect = () => {
+            if (index < fullText.length && !isDeleting) {
+                setDisplayText((prev) => prev + fullText.charAt(index));
+                index++;
+                setTimeout(typeEffect, typingSpeed);
+            } else if (index === fullText.length && !isDeleting) {
+                setTimeout(() => {
+                    isDeleting = true;
+                    typeEffect();
+                }, pauseTime);
+            } else if (index > 0 && isDeleting) {
+                setDisplayText((prev) => prev.slice(0, -1));
+                index--;
+                setTimeout(typeEffect, deletingSpeed);
+            } else if (index === 0 && isDeleting) {
+                isDeleting = false;
+                setTimeout(typeEffect, pauseTime);
+            }
+        };
+        typeEffect();
 
-		const cursorTimer = setInterval(() => {
-			setCursorVisible((prev) => !prev);
-		}, cursorBlinkSpeed);
+        const cursorTimer = setInterval(() => {
+            setCursorVisible((prev) => !prev);
+        }, cursorBlinkSpeed);
 
-		return () => clearInterval(cursorTimer);
-	}, []);
+        return () => clearInterval(cursorTimer);
+    }, []);
 
-	return (
-		<div className="home-container">
-			<div id="home" className="home-contai">
-				<div className="home-con1">
-					<h3 className="home-bienvenido">Bienvenido a mi Portafolio</h3>
-					<p className="home-texto">Hola, mi nombre es</p>
-					<h1 className="home-titulo">Luis Castilla</h1>
-					<p className="home-texto">Y soy </p>
-					<h2 className="home-full">
-						{displayText}
-						{cursorVisible && <span className="cursor">|</span>}
-					</h2>
-					<h6 className="home-intro">
-						Apasionado por la programación y desarrollo web, con experiencia en tecnologías como HTML, CSS, JavaScript, React, Node.js, Python, SQL, SQLAlchemy, Bootstrap, Git, GitHub, Flask, Authentication JWT y Jest.
-					</h6>
-					<div>
-						<a href="/CV_Luis_Castilla.pdf" download="CV_Luis_Castilla.pdf">
-							<button className="contact-button">Descargar CV</button>
-						</a>
-					</div>
-				</div>
-				<div className="home-perfil">
-					<div className="home-con2">
-						<img className="foto-perfil" src={foto} alt="Luis Castilla" />
-					</div>
-					<div className="home-redes">
-						<a href="https://www.linkedin.com/in/luiscastillav30/" target="_blank" rel="noopener noreferrer">
-							<div className="icon-container">
-								<FontAwesomeIcon icon={faLinkedin} className="redes-icono" />
-							</div>
-						</a>
-						<a href="https://github.com/luiscastillaPro" target="_blank" rel="noopener noreferrer">
-							<div className="icon-container">
-								<FontAwesomeIcon icon={faGithub} className="redes-icono" />
-							</div>
-						</a>
-						<a href="https://www.instagram.com/luis.castillav30" target="_blank" rel="noopener noreferrer">
-							<div className="icon-container">
-								<FontAwesomeIcon icon={faInstagram} className="redes-icono" />
-							</div>
-						</a>
-						<a href="https://www.tiktok.com/@luiscastilla" target="_blank" rel="noopener noreferrer">
-							<div className="icon-container">
-								<FontAwesomeIcon icon={faFacebook} className="redes-icono" />
-							</div>
-						</a>
-					</div>
-				</div>
-			</div>
+    useEffect(() => {
+        const sections = document.querySelectorAll(".fade-in-element");
 
-			{/* Sección de Skills */}
-			<section id="skills">
-				<Skills />
-			</section>
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                    // Dejar de observar una vez que el elemento es visible
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, { threshold: 0.1 }); // El 10% del elemento debe ser visible
 
-			{/* Agrega otras secciones aquí si es necesario, como "Proyectos" o "Contacto" */}
-		</div>
-	);
+        sections.forEach(section => {
+            observer.observe(section);
+        });
+
+        return () => {
+            sections.forEach(section => {
+                observer.unobserve(section);
+            });
+        };
+    }, []);
+
+    return (
+        <div className="home-container fade-in-element">
+            <div className="home-contai fade-in-element">
+                <div className="home-con1 fade-in-element">
+                    <h3 className="home-bienvenido fade-in-element">Bienvenido a mi Portafolio</h3>
+                    <p className="home-texto fade-in-element">Hola, mi nombre es</p>
+                    <h1 className="home-titulo fade-in-element">Luis Castilla</h1>
+                    <p className="home-texto fade-in-element">Y soy </p>
+                    <h2 className="home-full fade-in-element">
+                        {displayText}
+                        {cursorVisible && <span className="cursor">|</span>}
+                    </h2>
+                    <h6 className="home-intro fade-in-element">
+                        Apasionado por la programación y desarrollo web, con experiencia en tecnologías como HTML, CSS, JavaScript, React, Node.js, Python, SQL, SQLAlchemy, Bootstrap, Git, GitHub, Flask, Authentication JWT y Jest.
+                    </h6>
+                    <div>
+                        <a href="/CV_Luis_Castilla.pdf" download="CV_Luis_Castilla.pdf">
+                            <button className="contact-button fade-in-element">Descargar CV</button>
+                        </a>
+                    </div>
+                </div>
+                <div className="home-perfil fade-in-element">
+                    <div className="home-con2">
+                        <img className="foto-perfil" src={foto} alt="Luis Castilla" />
+                    </div>
+                    <div className="home-redes">
+                        <a href="https://www.linkedin.com/in/luiscastillav30/" target="_blank" rel="noopener noreferrer">
+                            <div className="icon-container fade-in-element">
+                                <FontAwesomeIcon icon={faLinkedin} className="redes-icono" />
+                            </div>
+                        </a>
+                        <a href="https://github.com/luiscastillaPro" target="_blank" rel="noopener noreferrer">
+                            <div className="icon-container fade-in-element">
+                                <FontAwesomeIcon icon={faGithub} className="redes-icono" />
+                            </div>
+                        </a>
+                        <a href="https://www.instagram.com/luis.castillav30" target="_blank" rel="noopener noreferrer">
+                            <div className="icon-container fade-in-element">
+                                <FontAwesomeIcon icon={faInstagram} className="redes-icono" />
+                            </div>
+                        </a>
+                        <a href="https://www.tiktok.com/@luiscastilla" target="_blank" rel="noopener noreferrer">
+                            <div className="icon-container fade-in-element">
+                                <FontAwesomeIcon icon={faFacebook} className="redes-icono" />
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <section id="skills">
+                <Skills />
+            </section>
+        </div>
+    );
 };
 
 export default Home;
